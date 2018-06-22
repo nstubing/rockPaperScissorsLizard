3f60528c1bd165b0c925df51b92474c4dc9d4504
nstubing <n.stubing886@gmail.com>
2018-06-22 07:55:56 -0500

    finished game

--- rockPaperScissors/Game.cs
+++ rockPaperScissors/Game.cs
@@ -13,25 +13,30 @@ namespace rockPaperScissors
         //string inputTwo;
         Player player1;
         Player player2;
-        public string[] gestureOptions = { "rock", "paper", "scissors", "spock", "lizard" };
-      //  public string[,] gestureOutcomes = { { "crushes", "crushes" },{ "covers", "disproves" },{ "cuts", "decapitates" },{ "disassembles", "vaporizes" },{ "poisons", "eats" } };
-        public int[] calculationOptions = { 4, 0, 1, 2, 3, 4, 0, 1 };
+        public string[] gestureOptions;
+        public string[,] gestureOutcomes;
+        public int[] calculationOptions;
 
         //member constructor
         public Game()
         {
             player1 = new Human("Player One");
+            gestureOptions=new string[] { "rock", "paper", "scissors", "spock", "lizard" };
+            gestureOutcomes=new string [,] { { "", "", "crushes", "", "crushes" }, { "covers", "", "", "disproves", "" }, { "", "cuts", "", "", "decapitates" }, { "vaporizes", "", "disassembles", "", "" }, { "", "eats", "", "poisons", "" } };
+            calculationOptions=new int[] { 4, 0, 1, 2, 3, 4, 0, 1 };
         }
 
         //member methods
         public void DisplayRules()
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Welcome to Rock, Paper, Scissors, Lizard, Spock! You may play against a computer or another human. The rules are simple, " + Environment.NewLine+
                 "Rock crushes scissors and Rock squishes Lizard" + Environment.NewLine+
                 "Paper covers Rock and Paper disproves Spock" + Environment.NewLine+
                 "Scissors cuts Paper and Scissors cuts Lizard" + Environment.NewLine+
                 "Lizard Eats Paper and Lizard poisons Spock" + Environment.NewLine+
-                "Spock disassembles Scissors and Spock vaporizes Rock.");
+                "Spock disassembles Scissors and Spock vaporizes Rock." + Environment.NewLine);
+            Console.ForegroundColor = ConsoleColor.White;
         }
         public void GetPlayerTwo()
         {
@@ -77,14 +82,14 @@ namespace rockPaperScissors
         public void FindWinner()
         {
 
-            if ((player1.pick==calculationOptions[player1.pick])||(player1.pick==calculationOptions[player1.pick+3]))
+            if ((player2.pick==calculationOptions[player1.pick])||(player2.pick==calculationOptions[player1.pick+3]))
             {
-                Console.WriteLine("Player one wins!  "+gestureOptions[player1.pick]+" beats "+gestureOptions[player2.pick]+"!");
+                Console.WriteLine("Player one wins!  "+gestureOptions[player1.pick]+" "+gestureOutcomes[player1.pick,player2.pick]+" "+gestureOptions[player2.pick]+"!");
                 player1.wins += 1;
             }
             else
             {
-                Console.WriteLine("Player two wins!  " + gestureOptions[player2.pick] + " beats " + gestureOptions[player1.pick]+"!");
+                Console.WriteLine("Player two wins!  " + gestureOptions[player2.pick] + " " + gestureOutcomes[player2.pick, player1.pick] + " " + gestureOptions[player1.pick]+"!");
                 player2.wins += 1;
             }
         }
@@ -96,11 +101,11 @@ namespace rockPaperScissors
         {
             if(player1.wins==2)
             {
-                Console.WriteLine("Player One is the winner of Rock, Paper, Scissors, Lizard, Spock with 2 wins! against Player Two");
+                Console.WriteLine("Player One is the winner of Rock, Paper, Scissors, Lizard, Spock with 2 wins against Player Two!");
             }
             else
             {
-                Console.WriteLine("Player Two is the winner of Rock, Paper, Scissors, Lizard, Spock with 2 wins! against Player One");
+                Console.WriteLine("Player Two is the winner of Rock, Paper, Scissors, Lizard, Spock with 2 wins against Player One!");
             }
         }
 
